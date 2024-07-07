3fc71b40d55effb991d7a34fdef50696e076deb3
Dominic009 <dpg11000@gmail.com>
2024-07-07 11:24:24 +0600

    payment been successfull from server side request

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const bodyParser = require('body-parser');
+const axios  = require("axios");
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB Connection
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -88,6 +93,10 @@ async function run() {
         ship_state: "Dhaka",
         ship_postcode: "1000",
         ship_country: "Bangladesh",
+        shipping_method: 'Air',
+        product_name: "Lenovo",
+        product_category: "Electronic ",
+        product_profile:"general",
         multi_card_name: "mastercard,visacard,amexcard",
         value_a: "ref001_A",
         value_b: "ref002_B",
@@ -95,11 +104,23 @@ async function run() {
         value_d: "ref004_D",
       };
 
-      res.send("Got it");
+        const response = await axios({
+        method: "POST",
+        url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+        data: paymentDetails,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+
+      res.send({
+        paymentURL: response.data.GatewayPageURL
+      })
     });
 
     app.post("./sucess-payment", async (req, res) => {
-      res.send('Payment is succesfull')
+      const successData = req.body;
+      res.send('Payment is succesfull', successData)
     });
 
     // Send a ping to confirm a successful connection
---
+++ Server/readme.md
@@ -0,0 +1,4 @@
+Steps to implement SSLCommerz payment gateway
+#1. Initiate Shop in SSLCOMMERZ website and get store ID and Pass
+#2. Make an api route in the server side to do payment works ('/payment')
+#3. Make a function (axios{ method: "POST" }) is the client side to hit the payment route on the server side
